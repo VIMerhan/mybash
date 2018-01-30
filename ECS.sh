80687f56795730e85d0b3900ce24fd0f53c1770f
acke <vimer7575216574@gmail>
2018-01-30 16:56:38 +0800

    init

--- ECS.sh
+++ ECS.sh
@@ -5,5 +5,14 @@ elif [ $1 = ali ]; then
 ssh root@120.79.10.25
 elif [ $1 = ss ]; then
 ssh root@172.104.71.112
+elif [ $1 = pi ]
+then
+    ssh pi@192.168.0.8
+elif [ $1 = 3328 ]
+then
+    ssh firefly@192.168.0.9
+elif [ $1 = 98 ]
+then
+    ssh android@192.168.0.98
 else echo "请输入正确的云服务器"
 fi
