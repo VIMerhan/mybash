8da2fe61ac195ba1f8bedc53ab5450860dbdbcb0
acke <vimer7575216574@gmail>
2018-02-08 17:26:32 +0800

    modify

--- cp_3328.sh
+++ cp_3328.sh
@@ -2,7 +2,7 @@
 cp ~/work/HaierSmartAirCondition/include/ui/widget.h ~/
 cp ~/work/HaierSmartAirCondition/src/ui/widget.cpp ~/
 
-cp ~/test/build-untitled123-Desktop_Qt_5_9_1_GCC_64bit-Debug/ui_widget.h ~/
+cp ~/work/QT/build-untitled123-Desktop_Qt_5_9_1_GCC_64bit-Debug/ui_widget.h ~/
 
 scp ~/widget.h ~/widget.cpp ~/ui_widget.h firefly@192.168.0.$1:/home/firefly
 
