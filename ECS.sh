6a61b0be17ff0bf59f221de0e021688acc299a1d
acke <vimer7575216574@gmail>
2018-03-01 10:32:43 +0800

    add CI-tools

--- ECS.sh
+++ ECS.sh
@@ -2,7 +2,7 @@
 if [ $1 = ten ]; then
 ssh root@123.207.126.120
 elif [ $1 = ali ]; then
-ssh root@120.79.10.25
+ssh root@120.79.173.163
 elif [ $1 = ss ]; then
 ssh root@172.104.71.112
 elif [ $1 = pi ]
@@ -10,7 +10,7 @@ then
     ssh ubuntu@192.168.0.92
 elif [ $1 = 3328 ]
 then
-    ssh firefly@192.168.0.9
+    ssh firefly@192.168.0.168
 elif [ $1 = 98 ]
 then
     ssh android@192.168.0.98
---
+++ cpp_style_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ~/mybash/styleguide/cpplint/cpplint.py *
